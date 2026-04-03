955c42db4f3ae0f0e23ed5d32af56c51f0c9c007
proyectotureporte <proyectotureporte@outlook.com>
2026-04-03 16:13:07 +0200

    Update transfer-dialog.tsx

--- src/components/dashboard/transfer-dialog.tsx
+++ src/components/dashboard/transfer-dialog.tsx
@@ -171,12 +171,15 @@ export function TransferDialog({ onSuccess, refreshMode, transactions: externalT
         if (!value) resetForm();
       }}
     >
-      <DialogTrigger asChild>
-        <Button>
-          <Plus className="mr-2 h-4 w-4" />
-          Nueva Transferencia
-        </Button>
-      </DialogTrigger>
+     <DialogTrigger asChild>
+  <Button
+    disabled
+    className="bg-gray-400 hover:bg-gray-400 text-white cursor-not-allowed"
+  >
+    <Plus className="mr-2 h-4 w-4" />
+    Nueva Transferencia
+  </Button>
+</DialogTrigger>
       <DialogContent
         onPointerDownOutside={(e) => { if (success) e.preventDefault(); }}
         onEscapeKeyDown={(e) => { if (success) e.preventDefault(); }}
